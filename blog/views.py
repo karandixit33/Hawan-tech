52892289f3c1ce97f25bac8bf9e47809e0eae31a
Your Name <your-email@example.com>
2025-03-17 23:07:31 +0530

    .gitignore file

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Blog
+from .serializers import BlogSerializer
+
+class BlogListCreateView(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class BlogDetailView(generics.RetrieveDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
